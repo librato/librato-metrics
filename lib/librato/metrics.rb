8e515d40ebee7063822cb0416f32b40dfa232d12
Matt Sanders <matt@librato.com>
2011-12-07 10:34:19 -0600

    Initial query support.

--- lib/librato/metrics.rb
+++ lib/librato/metrics.rb
@@ -23,7 +23,57 @@ module Librato
                   :authenticate, :connection, :persistence, :persistence=,
                   :persister, :submit
 
-    def self.list(args)
+    # Query metric data
+    #
+    # @example Get attributes for a metric
+    #   attrs = Librato::Metrics.fetch :temperature
+    #
+    # @example Get 20 most recent data points for metric
+    #   data = Librato::Metrics.fetch :temperature, :count => 20
+    #
+    # @example Get 20 most recent data points for a specific source
+    #   data = Librato::Metrics.fetch :temperature, :count => 20,
+    #                                  :source => 'app1'
+    #
+    # @example Get the 20 most recent 15 minute data point rollups
+    #   data = Librato::Metrics.fetch :temperature, :count => 20,
+    #                                 :resolution => 900
+    #
+    # A full list of query parameters can be found in the API
+    # documentation: {http://dev.librato.com/v1/get/gauges/:name}
+    #
+    # @param [Symbol|String] metric Metric name
+    # @param [Hash] options Query options
+    def self.fetch(metric, options={})
+      resolution = options.delete(:resolution) || 1
+      count = options.delete(:count)
+      query = {}
+      if count
+        query.merge!({:count => count, :resolution => resolution})
+      end
+      query.merge!(options)
+      response = connection.get(:path => "v1/gauges/#{metric}.json",
+                                :query => query, :expects => 200)
+      parsed = JSON.parse(response.body)
+      query.empty? ? parsed : parsed["measurements"]
+    end
+
+    # List currently existing metrics
+    #
+    # @example List all metrics
+    #   Librato::Metrics.list
+    #
+    # @example List metrics with 'foo' in the name
+    #   Librato::Metrics.list :name => 'foo'
+    #
+    # @param [Hash] options
+    def self.list(options={})
+      query = {}
+      query[:name] = options[:name] if options[:name]
+      response = connection.get(:path => 'v1/metrics.json',
+                                :query => query, :expects => 200)
+      # TODO: pagination support
+      JSON.parse(response.body)["metrics"]
     end
 
   end

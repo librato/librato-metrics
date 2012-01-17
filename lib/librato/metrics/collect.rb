a22520ab35adcf635c15b786b2c2f3967a0db0e1
Nuno Valente <nuno.valente@gmail.com>
2012-01-17 00:40:32 +0000

    Cleaned up paginated_metrics method

--- lib/librato/metrics/collect.rb
+++ lib/librato/metrics/collect.rb
@@ -4,9 +4,9 @@ module Librato
 
       MAX_RESULTS = 100
 
-      # Aggregates all results of paginated elements requesting more collections as needed
+      # Aggregates all results of paginated elements, requesting more collections as needed
       #
-      # @param [Excon] connection connection to Metrics service
+      # @param [Excon] connection Connection to Metrics service
       # @param [String] path API uri
       # @param [Hash] query Query options
       def self.paginated_metrics connection, path, query
@@ -17,13 +17,13 @@ module Librato
         results = parsed["metrics"]
         return results if parsed["query"]["found"] <= MAX_RESULTS
         query[:offset] = MAX_RESULTS
-        while query[:offset] < parsed["query"]["found"]
+        begin
           response = connection.get(:path => path,
                                     :query => query, :expects => 200)
           parsed = JSON.parse(response.body)
           results.push(*parsed["metrics"])
-          query[:offset] = query[:offset] + MAX_RESULTS
-        end
+          query[:offset] += MAX_RESULTS
+        end while query[:offset] < parsed["query"]["found"]
         results
       end
 

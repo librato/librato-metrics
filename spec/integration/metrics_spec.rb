396ac62ae9c2ea6e1c58c8cc06baccfaa323a2b7
Matt Sanders <matt@librato.com>
2012-02-15 11:46:25 -0600

    Add basic integration tests for submit.

--- spec/integration/metrics_spec.rb
+++ spec/integration/metrics_spec.rb
@@ -4,11 +4,14 @@ module Librato
   describe Metrics do
     before(:all) { prep_integration_tests }
 
+    pending "#fetch" do
+    end
+
     describe "#list" do
 
       before(:all) do
         delete_all_metrics
-        Librato::Metrics.submit :foo => 123, :bar => 345, :baz => 678, :foo_2 => 901
+        Metrics.submit :foo => 123, :bar => 345, :baz => 678, :foo_2 => 901
       end
 
       context "without arguments" do
@@ -30,6 +33,49 @@ module Librato
       end
 
     end
+
+    describe "#submit" do
+
+      context "with a gauge" do
+        before(:all) do
+          delete_all_metrics
+          Metrics.submit :foo => 123
+        end
+
+        it "should create the metrics" do
+          metric = Metrics.list[0]
+          metric['name'].should == 'foo'
+          metric['type'].should == 'gauge'
+        end
+
+        it "should store their data" do
+          data = Metrics.fetch :foo, :count => 1
+          data.should_not be_empty
+          data['unassigned'][0]['value'] == 123.0
+        end
+      end
+
+      context "with a counter" do
+        before(:all) do
+          delete_all_metrics
+          Metrics.submit :bar => {:type => :counter, :source => 'baz', :value => 456}
+        end
+
+        it "should create the metrics" do
+          metric = Metrics.list[0]
+          metric['name'].should == 'bar'
+          metric['type'].should == 'counter'
+        end
+
+        it "should store their data" do
+          data = Metrics.fetch :bar, :count => 1
+          data.should_not be_empty
+          data['baz'][0]['value'] == 456.0
+        end
+      end
+
+    end
+
 
   end
 end

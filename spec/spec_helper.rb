2ed4da6108b77ec4c2531f1c06f43e5295766b99
Matt Sanders <matt@librato.com>
2012-02-15 11:06:53 -0600

    Add helpers to setup integration testing environment, delete all metrics.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,23 @@ require 'librato/metrics'
 
 RSpec.configure do |config|
 
+  # set up test account credentials for integration tests
+  def prep_integration_tests
+    raise 'no TEST_API_USER specified in environment' unless ENV['TEST_API_USER']
+    raise 'no TEST_API_KEY specified in environment' unless ENV['TEST_API_KEY']
+    if ENV['TEST_API_ENDPOINT']
+      Librato::Metrics.api_endpoint = ENV['TEST_API_ENDPOINT']
+    end
+    Librato::Metrics.authenticate ENV['TEST_API_USER'], ENV['TEST_API_KEY']
+  end
+
+  # purge all metrics from test account
+  def delete_all_metrics
+    connection = Librato::Metrics.connection
+    Librato::Metrics.list.each do |metric|
+      #puts "deleting #{metric['name']}..."
+      connection.delete(:path => "v1/metrics/#{metric['name']}", :expects => 204)
+    end
+  end
+
 end

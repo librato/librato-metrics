cddd8557530a9f0bd6df5d931e042e40d66f0543
Matt Sanders <matt@librato.com>
2012-03-26 16:04:36 -0500

    Add partial adapter detection.

--- lib/librato/metrics/connection.rb
+++ lib/librato/metrics/connection.rb
@@ -22,14 +22,15 @@ module Librato
       end
       
       def transport
+        raise NoClientProvided unless @client
         @transport ||= Faraday::Connection.new(:url => api_endpoint) do |f|
           #f.use FaradayMiddleware::EncodeJson
           f.adapter Faraday.default_adapter
           f.use Faraday::Response::RaiseError
           #f.use FaradayMiddleware::ParseJson, :content_type => /\bjson$/
         end.tap do |transport|
+          transport.headers[:user_agent] = user_agent
           transport.headers[:content_type] = 'application/json'
-          raise NoClientProvided unless @client
           transport.basic_auth @client.email, @client.api_key
         end
       end
@@ -44,15 +45,40 @@ module Librato
         end
         ua_chunks << "librato-metrics/#{Metrics::VERSION}"
         ua_chunks << "(#{ruby_engine}; #{RUBY_VERSION}p#{RUBY_PATCHLEVEL}; #{RUBY_PLATFORM})"
-        ua_chunks << "direct-excon/#{1}"
+        ua_chunks << "direct-faraday/#{Faraday::VERSION}"
+        # TODO: include adapter information
+        #ua_chunks << "(#{transport_adapter})"
         ua_chunks.join(' ')
       end
       
     private
 
+      def adapter_version
+        adapter = transport_adapter
+        case adapter
+        when "NetHttp"
+          "Net::HTTP/#{Net::HTTP::Revision}"
+        when "Typhoeus"
+          "typhoeus/#{Typhoeus::VERSION}"
+        when "Excon"
+          "excon/#{Excon::VERSION}"
+        else
+          adapter
+        end
+      end
+
       def ruby_engine
         return RUBY_ENGINE if Object.constants.include?(:RUBY_ENGINE)
         RUBY_DESCRIPTION.split[0]
+      end
+      
+      # figure out which adapter faraday is using
+      def transport_adapter
+        transport.builder.handlers.each do |handler|
+          if handler.name[0,16] == "Faraday::Adapter"
+            return handler.name[18..-1]
+          end 
+        end
       end
       
     end

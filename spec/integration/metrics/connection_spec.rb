c10a3721ef92842571b9fc8b111e1b566c5cd1ed
Matt Sanders <matt@librato.com>
2012-03-26 17:04:01 -0500

    Add stub testfile for connection integration testing.

---
+++ spec/integration/metrics/connection_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+module Librato
+  module Metrics
+   
+    describe Connection do
+      
+      # TODO: test retry support
+      # TODO: test status code support
+      
+    end
+    
+  end
+end

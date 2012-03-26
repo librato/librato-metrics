b34cb0e4976bee87f517cabbdda568f9157b48dc
Matt Sanders <matt@librato.com>
2012-03-26 17:01:19 -0500

    Clean up retry middleware.

--- lib/librato/metrics/middleware/retry.rb
+++ lib/librato/metrics/middleware/retry.rb
@@ -5,22 +5,20 @@ module Librato
       
       class Retry < Faraday::Middleware
         
-        def initialize(app, retries = 2)
+        def initialize(app, retries = 3)
           @retries = retries
           super(app)
         end
 
         def call(env)
           retries = @retries
-          begin
-            @app.call(env)
-          rescue StandardError, Timeout::Error
-            if retries > 0
-              retries -= 1
-              retry
-            end
-            raise
+          @app.call(env)
+        rescue StandardError, Timeout::Error
+          if retries > 0
+            retries -= 1
+            retry
           end
+          raise
         end
         
       end

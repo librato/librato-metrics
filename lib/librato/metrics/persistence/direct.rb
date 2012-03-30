03c23d39a392404cb795d7a5fc8c915d890e37b4
Matt Sanders <matt@librato.com>
2012-03-30 17:45:11 -0500

    Queues auto-chunk large numbers of measurements into multiple-requests.

--- lib/librato/metrics/persistence/direct.rb
+++ lib/librato/metrics/persistence/direct.rb
@@ -9,10 +9,42 @@ module Librato
         # Persist the queued metrics directly to the
         # Metrics web API.
         #
-        def persist(client, queued)
-          payload = MultiJson.encode(queued)
-          # expects 200
-          client.connection.post('metrics', payload)
+        def persist(client, queued, options={})
+          per_request = options[:per_request]
+          if per_request
+            requests = chunk_queued(queued, per_request)
+          else
+            requests = [queued]
+          end
+          requests.each do |request|
+            payload = MultiJson.encode(request)
+            # expects 200
+            client.connection.post('metrics', payload)
+          end
+        end
+        
+      private
+        
+        def chunk_queued(queued, per_request)
+          return [queued] if queue_count(queued) <= per_request
+          reqs = []
+          queued.each do |metric_type, measurements|
+            if measurements.size <= per_request
+              # we can fit all of this metric type in a single
+              # request, so do so
+              reqs << {metric_type => measurements}
+            else
+              # going to have to split things up
+              measurements.each_slice(per_request) do |elements|
+                reqs << {metric_type => elements}
+              end
+            end
+          end
+          reqs
+        end
+        
+        def queue_count(queued)
+          queued.inject(0) { |result, data| result + data.last.size }
         end
 
       end

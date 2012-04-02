eece3eb116f65ad0fe34af197e5687bd4fce7ee2
Matt Sanders <matt@librato.com>
2012-04-02 12:36:57 -0500

    Improve order of connection specs.

--- spec/unit/metrics/connection_spec.rb
+++ spec/unit/metrics/connection_spec.rb
@@ -5,49 +5,6 @@ module Librato
 
     describe Connection do
       
-      describe "network operations" do
-        context "when missing client" do
-          it "should raise exception" do
-            lambda { subject.get 'metrics' }.should raise_error(NoClientProvided)
-          end
-        end
-        
-        context "with 500 class errors" do
-          it "should retry" do
-            Middleware::CountRequests.reset
-            client = Client.new
-            client.api_endpoint = 'http://127.0.0.1:9296'
-            client.authenticate 'foo', 'bar'
-            with_rackup('status.ru') do
-              #binding.pry
-              lambda {
-                client.connection.transport.post 'service_unavailable'
-              }.should raise_error(ServerError)
-            end
-            Middleware::CountRequests.total_requests.should == 4
-          end
-        end
-        
-        context "with 400 class errors" do
-          it "should not retry" do
-            Middleware::CountRequests.reset
-            client = Client.new
-            client.api_endpoint = 'http://127.0.0.1:9296'
-            client.authenticate 'foo', 'bar'
-            with_rackup('status.ru') do
-              #binding.pry
-              lambda {
-                client.connection.transport.post 'not_found'
-              }.should raise_error(NotFound)
-              lambda {
-                client.connection.transport.post 'forbidden'
-              }.should raise_error(ClientError)
-            end
-            Middleware::CountRequests.total_requests.should == 2
-          end
-        end
-      end
-      
       describe "#api_endpoint" do
         context "when not provided" do
           it "should be default" do
@@ -83,6 +40,48 @@ module Librato
         # TODO: verify user agent is being sent with rackup test
       end
       
+      describe "network operations" do
+        context "when missing client" do
+          it "should raise exception" do
+            lambda { subject.get 'metrics' }.should raise_error(NoClientProvided)
+          end
+        end
+        
+        context "with 400 class errors" do
+          it "should not retry" do
+            Middleware::CountRequests.reset
+            client = Client.new
+            client.api_endpoint = 'http://127.0.0.1:9296'
+            client.authenticate 'foo', 'bar'
+            with_rackup('status.ru') do
+              #binding.pry
+              lambda {
+                client.connection.transport.post 'not_found'
+              }.should raise_error(NotFound)
+              lambda {
+                client.connection.transport.post 'forbidden'
+              }.should raise_error(ClientError)
+            end
+            Middleware::CountRequests.total_requests.should == 2
+          end
+        end
+        
+        context "with 500 class errors" do
+          it "should retry" do
+            Middleware::CountRequests.reset
+            client = Client.new
+            client.api_endpoint = 'http://127.0.0.1:9296'
+            client.authenticate 'foo', 'bar'
+            with_rackup('status.ru') do
+              #binding.pry
+              lambda {
+                client.connection.transport.post 'service_unavailable'
+              }.should raise_error(ServerError)
+            end
+            Middleware::CountRequests.total_requests.should == 4
+          end
+        end
+      end
       
     end
     

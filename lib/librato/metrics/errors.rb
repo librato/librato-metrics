62801d64aca43d7d5c7c8ede84359894d807f810
Matt Sanders <matt@librato.com>
2012-04-02 12:26:03 -0500

    Add basics of Metrics-specific network exceptions.

--- lib/librato/metrics/errors.rb
+++ lib/librato/metrics/errors.rb
@@ -8,6 +8,13 @@ module Librato
     class AgentInfoMissing < MetricsError; end
     class NoMetricsQueued < MetricsError; end
     class NoClientProvided < MetricsError; end
+    
+    class NetworkError < StandardError; end
+    
+    class ClientError < NetworkError; end
+    class NotFound < ClientError; end
+    
+    class ServerError < NetworkError; end
 
   end
 end
